a4d245b3d445d804712451bbd378a7ac4168650d
supraditya <aditya300100@gmail.com>
2023-08-07 16:22:01 -0400

    Improved listrenderer readability

--- components/ListRenderer.js
+++ components/ListRenderer.js
@@ -1,28 +1,22 @@
 // Component to render lists as comma separated JSX Items
-// removeEnds=true, means remove the first item and the term "Others" from the rendered list
+// removeEnds=true, means remove the first item (usually "All") and the term "Others" from the rendered list view
 
 export default function ListRenderer({ list, removeEnds }) {
-  return removeEnds
-    ? list[list.length-1] === "Others"
-      ? list.slice(1, -1).map((item, i) => {
-          return i !== list.length - 3 ? (
-            <span key={i}>{item}, </span>
-          ) : (
-            <span key={i}>{item}</span>
-          );
-        })
-      : list.slice(1).map((item, i) => {
-          return i !== list.length - 2 ? (
-            <span key={i}>{item}, </span>
-          ) : (
-            <span key={i}>{item}</span>
-          );
-        })
-    : list.map((item, i) => {
-        return i !== list.length - 1 ? (
-          <span key={i}>{item}, </span>
-        ) : (
-          <span key={i}>{item}</span>
-        );
-      });
+  let slicedList = [];
+  if (removeEnds) {
+    if (list[list.length - 1] === "Others") {
+      slicedList.push(...list.slice(1, -1));
+    } else {
+      slicedList.push(...list.slice(1));
+    }
+  } else {
+    slicedList.push(...list);
+  }
+  return slicedList.map((item, i) => {
+    return i !== slicedList.length - 1 ? (
+      <span key={i}>{item}, </span>
+    ) : (
+      <span key={i}>{item}</span>
+    );
+  });
 }
