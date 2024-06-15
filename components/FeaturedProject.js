1a146beed79e6d8a617e208ee31e71d84da61bcf
adityame <aditya300100@gmail.com>
2024-06-14 20:06:08 -0400

    Made the entire featured project and project card clickable

--- components/FeaturedProject.js
+++ components/FeaturedProject.js
@@ -1,45 +1,55 @@
 import Image from "next/image";
 import Link from "next/link";
 import ListRenderer from "./ListRenderer";
+import { useState } from "react";
 
 export default function FeaturedProject(props) {
+  const [hover, setHover] = useState(false);
   return (
-    <div
-      className={`py-4 ${
-        props.flow === "right" ? "md:flex-row-reverse" : ""
-      } flex max-md:flex-col-reverse justify-between md:items-center bg-transparent w-full md:mt-4 md:mb-8 min-h-[50vh]`}
+    <Link
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      href={`projects/${props.link}`}
+      alt="Link to project"
     >
       <div
-        className={`md:w-[48%] border-black ${
-          props.flow === "right" ? "md:border-l" : "md:border-r"
-        } py-6 h-full`}
+        className={`py-4 ${
+          props.flow === "right" ? "md:flex-row-reverse" : ""
+        } flex max-md:flex-col-reverse justify-between md:items-center bg-transparent w-full md:mt-4 md:mb-8 min-h-[50vh]`}
       >
         <div
-          className={`md:w-4/5 ${
-            props.flow === "right" ? "md:float-right" : ""
-          }`}
+          className={`md:w-[48%] border-black ${
+            props.flow === "right" ? "md:border-l" : "md:border-r"
+          } py-6 h-full`}
         >
-          <p className="font-primary text-2xl md:text-5xl mb-0.5 md:mb-2">
-            {props.title}
-          </p>
-          <div className="font-secondary font-medium text-lg md:text-xl">
-            <ListRenderer list={props.domains} removeEnds={true} />
-          </div>
-          <div className="font-secondary font-light text-md mb-2 text-subtitleGray">
-          {props.description}
-          </div>
-          <Link href={`projects/${props.link}`} alt="Link to project">
-            <button aria-label="View Project" className="font-primary text-base border border-black rounded-sm px-3 py-2 hover:bg-black hover:text-white ease-in-out">
+          <div
+            className={`md:w-4/5 ${
+              props.flow === "right" ? "md:float-right" : ""
+            }`}
+          >
+            <p className="font-primary text-2xl md:text-5xl mb-0.5 md:mb-2">
+              {props.title}
+            </p>
+            <div className="font-secondary font-medium text-lg md:text-xl">
+              <ListRenderer list={props.domains} removeEnds={true} />
+            </div>
+            <div className="font-secondary font-light text-md mb-2 text-subtitleGray">
+              {props.description}
+            </div>
+            <button
+              aria-label="View Project"
+              className={`font-primary text-base border border-black rounded-sm px-3 py-2 ${hover ? 'bg-black text-white':''} ease-in-out`}
+            >
               View
             </button>
-          </Link>
+          </div>
         </div>
+        <Image
+          src={require(`../public/assets/${props.image}/${props.image}.png`)}
+          alt="project image"
+          className="md:w-5/12"
+        />
       </div>
-      <Image
-        src={require(`../public/assets/${props.image}/${props.image}.png`)}
-        alt="project image"
-        className="md:w-5/12"
-      />
-    </div>
+    </Link>
   );
 }
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,28 +1,44 @@
 import ListRenderer from "./ListRenderer";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ProjectCard(props) {
+  const [hover, setHover] = useState(false);
   return (
-    <div className={`min-w-sm min-h-[400px] flex flex-col justify-between rounded-md shadow-xl p-4 w-full md:max-w-lg mt-4 ${props.domains.includes(props.currentTab) ? '':'hidden'}`}>
-      <Image
-        src={require(`../public/assets/${props.image}/${props.image}.png`)}
-        alt="project image"
-        className="md:w-fit rounded-sm m-auto"
-      />
-      <div className="flex justify-between items-center">
-        <div className="rounded-sm mr-2">
-          <p className="font-primary font-semibold text-2xl">{props.title}</p>
-          <div className="font-secondary text-base">
-          <ListRenderer list={props.domains} removeEnds={true}/>
+    <Link
+      href={`projects/${props.link}`}
+      alt="Link to project"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div
+        className={`min-w-sm min-h-[400px] flex flex-col justify-between rounded-md shadow-xl p-4 w-full md:max-w-lg mt-4 ${
+          props.domains.includes(props.currentTab) ? "" : "hidden"
+        }`}
+      >
+        <Image
+          src={require(`../public/assets/${props.image}/${props.image}.png`)}
+          alt="project image"
+          className="md:w-fit rounded-sm m-auto"
+        />
+        <div className="flex justify-between items-center">
+          <div className="rounded-sm mr-2">
+            <p className="font-primary font-semibold text-xl">{props.title}</p>
+            <div className="font-secondary text-base">
+              <ListRenderer list={props.domains} removeEnds={true} />
+            </div>
           </div>
+          <button
+            aria-label="View project"
+            className={`font-primary text-base border border-black rounded-sm px-3 py-2 ${
+              hover ? "bg-black text-white" : ""
+            } ease-in-out`}
+          >
+            View
+          </button>
         </div>
-        <Link href={`projects/${props.link}`} alt="Link to project">
-            <button aria-label="View project" className="font-primary text-base border border-black rounded-sm px-3 py-2 hover:bg-black hover:text-white ease-in-out">
-              View
-            </button>
-          </Link>
       </div>
-    </div>
+    </Link>
   );
 }
